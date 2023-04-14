2d28fc627a23323e9a61ce9b43858f6292aad3d3
Stuart Frew stuart.s.frew@gmail.com <stuart.s.frew@gmail.com>
2023-04-14 18:42:55 +0100

    database initialisation
    created migration file to create activities table. Created activity model. Set relationship between user -> activity

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_14_095639) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_14_161415) do
+  create_table "activities", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_activities_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password"
@@ -18,4 +27,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_14_095639) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "activities", "users"
 end
