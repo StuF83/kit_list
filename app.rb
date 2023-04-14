0131b39c087f5b979d144546fb13626edd165b8a
Stuart Frew stuart.s.frew@gmail.com <stuart.s.frew@gmail.com>
2023-04-14 16:51:47 +0100

    initial database building
    .gitignore changed to include only .sqlite3 database file. db:migrate file created for users. User model created

--- app.rb
+++ app.rb
@@ -2,9 +2,13 @@
 
 require 'sinatra'
 require 'sinatra/activerecord'
+require_relative 'models/user.rb'
 
 set :database, {adapter: "sqlite3", database: 'db/development.sqlite3'}
 
 get '/' do
   "Hello World"
 end
+
+user = User.new(email: "stu@stu.frew")
+p user
