a77e6829851eb0672ea4b6acf11cfd271aaa46da
Stuart Frew stuart.s.frew@gmail.com <stuart.s.frew@gmail.com>
2023-04-15 18:01:14 +0100

    item database building
    added item migration and model. Seed file initial population to test items can be accessed through activites and users.

--- app.rb
+++ app.rb
@@ -4,9 +4,18 @@ require 'sinatra'
 require 'sinatra/activerecord'
 require_relative 'models/user.rb'
 require_relative 'models/activity.rb'
+require_relative 'models/item.rb'
 
 set :database, {adapter: "sqlite3", database: 'db/development.sqlite3'}
 
 get '/' do
-  "Hello World"
+  erb :index
+end
+
+post '/new_activity' do
+  activity = params[:activity]
+  user = User.first
+  Activity.create(name: activity, user: user)
+  redirect back
+  erb :index
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_14_161415) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_15_161126) do
   create_table "activities", force: :cascade do |t|
     t.string "name"
     t.string "description"
@@ -18,6 +18,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_14_161415) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_activities_on_user_id"
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.integer "activity_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["activity_id"], name: "index_items_on_activity_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -28,4 +37,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_14_161415) do
   end
 
   add_foreign_key "activities", "users"
+  add_foreign_key "items", "activities"
 end
