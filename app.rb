daa14937d009d2b08a0d9f1d7309ac6571a671a7
Stuart Frew stuart.s.frew@gmail.com <stuart.s.frew@gmail.com>
2023-04-18 14:08:24 +0100

    js event listener on activity checkboxes now console logs the key - NAME, and the respective value (ie activity name) so that these can be appended to a get reqests

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@
 
 require 'sinatra'
 require 'sinatra/activerecord'
+require 'json'
 require_relative 'models/user.rb'
 require_relative 'models/activity.rb'
 require_relative 'models/item.rb'
@@ -18,4 +19,17 @@ post '/new_activity' do
   Activity.create(name: activity, user: user)
   redirect back
   erb :index
+end
+
+get '/item_request' do
+
+    # get an array of items
+  # we need js to send an http request with a post?? so we can get params?
+  # we can do this with a fetch() post request, we don't need to wait for .then as sinatra will be updating the page
+  # or do we return a json and use that to update the page? can we do this...? We'd need sinatra to return the json back to the js request, how can we do this?
+
+  user = User.first
+  activity = user.activities[0]
+  items = activity.items
+  items.to_json
 end
--- public/scripts.js
+++ public/scripts.js
@@ -4,6 +4,23 @@ const unorderedList = document.getElementById("ul");
 // const xmlHttpReq = new XMLHttpRequest
 
 unorderedList.addEventListener('change', (event) => {
-  console.log(event.target.checked);
-  console.log(event.target.name);
+  // console.log(event.target.checked);
+  // console.log(event.target.name);
+  // window.location.href = 'http://localhost:4567/item_request';
+  var data = { NAME: event.target.name}
+  var url = new URL("http://localhost:4567/item_request")
+  // console.log(data);
+  // console.log(url.searchParams)
+  Object.keys(data).forEach(key => {
+    console.log(key);
+    console.log(data[key]);
+  })
+
+
+
+  // fetch("http://localhost:4567/item_request")
+  // .then(response => response.json())
+  // .then((data) => {
+  //   console.log(data)
+  // })
 })
