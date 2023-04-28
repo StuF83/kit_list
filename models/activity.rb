268632a5d44e474b0a9ab61bfe24719556651e25
Stuart Frew stuart.s.frew@gmail.com <stuart.s.frew@gmail.com>
2023-04-28 12:23:27 +0100

    methods moved to models

--- models/activity.rb
+++ models/activity.rb
@@ -1,4 +1,10 @@
 class Activity < ActiveRecord::Base
   belongs_to :user
   has_many :items
+
+  def items_array
+    @item_list = []
+    items.each { |item| @item_list << item.name }
+    @item_list
+  end
 end
