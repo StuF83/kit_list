3c69e80433c2b7fbd7e08cf5a9dd9c41548ee504
Stuart Frew stuart.s.frew@gmail.com <stuart.s.frew@gmail.com>
2023-04-29 19:13:49 +0100

    session item array added
    a session items_to_pack array has been added. This allows us to keep track of the packing list as we select/deselect activites. Currently we can only add activities to packing list, not remove

--- app.rb
+++ app.rb
@@ -13,6 +13,11 @@ configure do
   enable :sessions
 end
 
+def initialize
+  super
+  @items_to_pack = []
+end
+
 get '/' do
   erb :index, locals: { user: session[:user] }
 end
@@ -27,19 +32,34 @@ post '/login' do
   redirect back unless User.exists?(email: session[:email])
   redirect back unless User.find_by(email: session[:email]).password == session[:password]
   session[:user] = User.find_by(email: session[:email])
+  session[:items_to_pack] ||= @items_to_pack
   redirect '/'
 end
 
 post '/new_activity' do
   activity = params[:activity]
-  user = User.first
-  user.addActivity(activity)
+  user = session[:user]
+  user.add_activity(activity)
   redirect back
 end
 
 get '/item_request' do
+  # what we need to do here is have an item list as an instance variable for the user.
+  # As the request comes in, we either add or remove the items from the list instance variable.
+  # the instance variable should be an array of arrays.
+  # the first item of the array is the acitivity name, the subsequent items are the 'items'.
+  # as the param[:name] comes in, we can compare the name against the first item in each array (ie activity name)
+  # and either add or subtract depending on it's existance.
   activity = params[:name]
-  user = User.first
-  activity = user.activities.find_by(name: activity)
-  activity.items_array.to_json
+  user = session[:user]
+  p activity_name = user.activities.find_by(name: activity).name
+  p activity_items = user.activities.find_by(name: activity).items_array
+  p @items_to_pack
+  p user.add_items_to_pack(activity_name, activity_items)
+  p @items_to_pack << user.add_items_to_pack(activity_name, activity_items)
+  @items_to_pack.to_json
+  # activity = user.activities.find_by(name: activity)
+  # p activity.items_array
+  # user.add_items_to_pack(activity.name, activity.items_array)
+  # p user.items_to_pack.to_json
 end
--- public/scripts.js
+++ public/scripts.js
@@ -3,19 +3,26 @@ const unorderedList = document.getElementById("ul");
 unorderedList.addEventListener('change', (event) => {
   var data = { name: event.target.name}
   var url = new URL("http://localhost:4567/item_request")
+  // console.log(event.target.checked);
+  // console.log(Object.keys(data));
   Object.keys(data).forEach(key => {
+    // console.log(key);
+    // console.log(data);
     url.searchParams.append(key, data[key])
   })
-  console.log(url)
 
   fetch(url)
   .then(response => response.json())
   .then((data) => {
-    console.log(data)
+    console.log(data);
     const itemList = document.getElementById("itemList");
-    console.log(itemList);
-    data.forEach(element => {
-      itemList.insertAdjacentHTML("beforeend", `<li>${element}</li>`)
+    while (itemList.hasChildNodes()) {
+      itemList.removeChild(itemList.firstChild);
+    }
+    data.forEach(activity => {
+      activity.forEach(element =>{
+        itemList.insertAdjacentHTML("beforeend", `<li>${element}</li>`)
+      })
     })
   })
 })
