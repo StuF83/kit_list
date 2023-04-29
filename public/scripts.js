8efec9ba061e5924294d89fff049ae80e782a390
Stuart Frew stuart.s.frew@gmail.com <stuart.s.frew@gmail.com>
2023-04-29 19:25:45 +0100

    scripts file comments tidied

--- public/scripts.js
+++ public/scripts.js
@@ -3,11 +3,7 @@ const unorderedList = document.getElementById("ul");
 unorderedList.addEventListener('change', (event) => {
   var data = { name: event.target.name}
   var url = new URL("http://localhost:4567/item_request")
-  // console.log(event.target.checked);
-  // console.log(Object.keys(data));
   Object.keys(data).forEach(key => {
-    // console.log(key);
-    // console.log(data);
     url.searchParams.append(key, data[key])
   })
 
