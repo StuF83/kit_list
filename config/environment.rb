0a6cefdba295e7326adde3c268fee9ee51ffe21e
Stuart Frew stuart.s.frew@gmail.com <stuart.s.frew@gmail.com>
2023-07-12 15:53:59 +0100

    yml update

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
+
+set :database_file, '.config/database.yml'
+
 configure do
   enable :sessions
 end
@@ -7,19 +10,9 @@ end
 #   set :database, { adapter: 'sqlite3', database: 'db/development.sqlite3' }
 # end
 
-configure :production do
-  ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
-  # Rails.application.config.after_initialize do
-  #   ActiveRecord::Base.connection_pool.disconnect!
+# configure :production do
 
-  #   ActiveSupport.on_load(:active_record) do
-  #     config =  ActiveRecord::Base.configurations[Rails.env] ||
-  #               Rails.application.config.database_configuration[Rails.env]
-  #     config['pool'] = ENV['DB_POOL'] || ENV['RAILS_MAX_THREADS'] || 5
-  #     ActiveRecord::Base.establish_connection(config) # Establish connection is not needed for Rails 5.2+ https://github.com/rails/rails/pull/31241
-  #   end
-  # end
-end
+# end
 
 #   db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
 #   ActiveRecord::Base.establish_connection(
